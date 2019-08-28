f4862c5b6b2d0322ae381b46dc3d1440afa6d33e
Anton Purver <270025112@col573-l13-18.local>
2019-08-28 20:16:29 +1200

    updated

---
+++ js/app.js
@@ -0,0 +1,24 @@
+
+// let hover = document.querySelector(".features__content-wrap")
+// let scale = document.querySelector(".features__background")
+
+// hover.addEventListener("mouseover", function(){
+//     scale.classList.add("features__background-scale")
+// })
+// hover.addEventListener("mouseleave", function(){
+//     scale.classList.remove("features__background-scale")
+// })
+
+// let hovers = document.querySelectorAll(".features__content-wrap")
+// let scale = document.querySelector(".features__background")
+
+// // hover.addEventListener("mouseover", function(){
+// //     scale.classList.add("features__background-scale")
+// // })
+
+
+// hovers.forEach((hover) => {
+//     hover.addEventListener('mouseover', () => {
+//         scale.classList.add("features__background-scale")
+//     });
+//   });

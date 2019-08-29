e4f0fea641533ba1d2aaf66af4d9c3e81d4fd8f1
Anton Purver <270025112@col573-l13-18.local>
2019-08-29 16:06:23 +1200

    Fixed js hover

--- js/app.js
+++ js/app.js
@@ -1,24 +1,12 @@
-
-// let hover = document.querySelector(".features__content-wrap")
-// let scale = document.querySelector(".features__background")
-
-// hover.addEventListener("mouseover", function(){
-//     scale.classList.add("features__background-scale")
-// })
-// hover.addEventListener("mouseleave", function(){
-//     scale.classList.remove("features__background-scale")
-// })
-
-// let hovers = document.querySelectorAll(".features__content-wrap")
-// let scale = document.querySelector(".features__background")
-
-// // hover.addEventListener("mouseover", function(){
-// //     scale.classList.add("features__background-scale")
-// // })
+let hovers = document.querySelectorAll(".features__content-wrap")
+let scales = document.querySelectorAll(".features__background")
 
 
-// hovers.forEach((hover) => {
-//     hover.addEventListener('mouseover', () => {
-//         scale.classList.add("features__background-scale")
-//     });
-//   });
+hovers.forEach((hover, i) => {
+    hover.addEventListener('mouseover', () => {
+        scales[i].classList.add("features__background-scale")
+    });
+    hover.addEventListener('mouseleave', () => {
+        scales[i].classList.remove("features__background-scale")
+    });
+  });
